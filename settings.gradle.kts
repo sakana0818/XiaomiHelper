4ac53e32b1320cb019df2e7914670eb85f998f4a
sakana0818 <120461466+sakana0818@users.noreply.github.com>
2025-01-22 09:14:50 +0800

    更新 settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,4 +23,9 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "XiaomiHelper"
-include(":app", ":blockmiui", ":hyperx-compose")
+include(":app", ":blockmiui", ":hyperx------BEGIN SSH SIGNATURE-----
+U1NIU0lHAAAAAQAAADMAAAALc3NoLWVkMjU1MTkAAAAgvhiaVIl3mMEFNt14UjrX7GdAjp
+bDySHS+hgFFjNDeg4AAAAHHNwb3NlZAAAAAAAAAAGc2hhNTEyAAAAUwAAAAtzc2gtZWQy
+NTUxOQAAAEA000vS6oquD9ru0qaJAhAFD7H7RV1Sj6jV1mP/wp2ToOYo80Kq5hc1rf/NmD
+VoJvjEpQ5Ia7VjKTSOr2pcIhEL
+-----END SSH SIGNATURE-----compose")
